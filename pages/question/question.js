7163430d9df80a4e905d1e30a3155ec68945a0f7
AIWMoon <zzy.hero@foxmail.com>
2022-03-23 15:32:43 +0800

    增加提示，展示[答题][知识]页面效果

--- pages/question/question.js
+++ pages/question/question.js
@@ -5,9 +5,20 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        Processing: '4',
+        oneItems: [
+            {value: '1',name: 'A.其他垃圾'},
+            {value: '2',name: 'B.有害垃圾'},
+            {value: '3',name: 'C.可回收物'},
+            {value: '4',name: 'D.厨余垃圾'},
+        ]
     },
-
+    change:function(e){
+        var that = this;
+        that.setData({
+            Processing:e.detail.value
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
